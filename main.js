1ee65ec32d5d4c50bf1c27a83781cfa7cdc6deaf
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-07-23 11:25:55 +0200

    defines the data structures needed to store product information, categories, selection state, cart items, and modal state.

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import Alpine from 'alpinejs';
+import { DataProducts, getCategories } from './Api';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+window.Alpine = Alpine;
 
-setupCounter(document.querySelector('#counter'))
+document.addEventListener('alpine:init', () => {
+    Alpine.data('products', () => ({
+        products: [],
+        originalProducts: [],
+        filteredProducts: [],
+        categories: [],
+        selectedCategory: '',
+        sorting: 'default',
+        loading: true,
+        cart: [],
+        modalOpen: false,
+        selectedProduct: {},
+        get cartCount() {
+            return this.cart.length;
+        },
