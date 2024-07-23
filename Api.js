fc41232cc51ebafb7d4a8635c8c8928def4ab91a
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-07-23 11:22:27 +0200

    function provides a robust way to fetch product category data from an API

--- Api.js
+++ Api.js
@@ -11,3 +11,17 @@ export async function DataProducts() {
       rating: product.rating,
     }));
   };
+  
+export const getCategories = async () => {
+    try {
+      const response = await fetch("https://fakestoreapi.com/products/categories");
+      if (!response.ok) {
+        throw new Error("Data fetching failed, please check your network connection");
+      }
+      const data = await response.json();
+      return { response: data };
+    } catch (error) {
+      return { error: error };
+    }
+  };
+  
